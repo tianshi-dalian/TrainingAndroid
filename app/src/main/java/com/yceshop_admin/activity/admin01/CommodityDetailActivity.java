eeca724f3ec994a5ee7a303aa8fe7a695c39dd3b
qinhp <qhp603560399@163.com>
2018-05-19 16:55:05 +0800

    add qinhp布局

---
+++ app/src/main/java/com/yceshop_admin/activity/admin01/CommodityDetailActivity.java
@@ -0,0 +1,21 @@
+package com.yceshop_admin.activity.admin01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yceshop_admin.R;
+/**
+ *
+ * CommodityDetailActivity
+ * 商品详情activity
+ * @author qinhp
+ * @time 2018/5/19
+ */
+public class CommodityDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commodity_detail);
+    }
+}
---
+++ app/src/main/java/com/yceshop_admin/activity/admin01/HomeActivity.java
@@ -0,0 +1,22 @@
+
+package com.yceshop_admin.activity.admin01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yceshop_admin.R;
+/**
+ * 
+ * HomeActivity
+ *
+ * @author qinhp
+ * @time 2018/5/19
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R .layout.item_commodity_list);
+    }
+}
